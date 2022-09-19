0d2f8d2edc7c753333300f2ec900893ecf3606dd
Rakin Rahman <rakinhacker@gmail.com>
2022-09-19 18:42:09 +0600

    Define Enemy struct component

--- include/components.h
+++ include/components.h
@@ -1,16 +1,28 @@
 #ifndef COMPONENTS_H
 #define COMPONENTS_H
 
+#include <SDL2/SDL.h>
+
+// Size component with width and height fields
 using size = struct
 {
     int width;
     int height;
 };
 
+// Position component with x and y axis fields
 using position = struct
 {
     int x;
     int y;
+};
+
+// Enemy component
+struct Enemy
+{
+    SDL_Rect box;
+    size size;
+    position position;
 };
 
 #endif
