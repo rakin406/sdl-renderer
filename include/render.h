f0f01e326b9104109fc13b3c28194a0e304a6fee
Rakin Rahman <rakinhacker@gmail.com>
2022-09-19 17:43:44 +0600

    Change struct

--- include/render.h
+++ include/render.h
@@ -2,9 +2,19 @@
 #define RENDER_H
 
 #include <SDL2/SDL.h>
+#include <entt/entt.hpp>
 
-struct SDL_Window;
-struct SDL_Renderer;
+#include "components.h"
+
+struct EnemyBox
+{
+    SDL_Rect enemy;
+    size size;
+    position position;
+};
+
+void drawEnemyBox(struct EnemyBox &enemy, SDL_Renderer *renderer);
+void drawEnemyBoxSDL(entt::registry &registry);
 
 bool initializeSDL();
 void clearScreen();
