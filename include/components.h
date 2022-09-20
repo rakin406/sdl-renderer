bd77c81892227040e9d6b5a84ac3061bdbdd80e2
Rakin Rahman <rakinhacker@gmail.com>
2022-09-20 18:31:54 +0600

    Remove some type aliases structs

--- include/components.h
+++ include/components.h
@@ -1,10 +1,6 @@
 #ifndef COMPONENTS_H
 #define COMPONENTS_H
 
-#include <SDL2/SDL.h>
-
-#include "utils.h"
-
 using Position = struct
 {
     int x;
@@ -16,33 +12,5 @@ using Size = struct
     int width;
     int height;
 };
-
-// Player position component
-struct PlayerPosition
-{
-    Position center;
-};
-
-// Player radius component
-struct PlayerRadius
-{
-    int32_t radius;
-};
-
-// Player component
-// struct Player
-// {
-//     Circle circle;
-//     PlayerPosition center;
-//     Radius radius;
-// };
-
-// Enemy component
-// struct Enemy
-// {
-//     SDL_Rect box;
-//     Position position;
-//     Size size;
-// };
 
 #endif
