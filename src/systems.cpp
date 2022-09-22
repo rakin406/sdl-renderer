e987eae6ad3613f86bd96a83ef74fe408eab4710
Rakin Rahman <rakinhacker@gmail.com>
2022-09-22 09:03:16 +0600

    Use callback function to update player

--- src/systems.cpp
+++ src/systems.cpp
@@ -4,6 +4,7 @@
 #include <entt/entt.hpp>
 
 #include "../include/components.h"
+#include "../include/constants.h"
 #include "../include/systems.h"
 
 namespace tools
@@ -36,59 +37,82 @@ Position getMousePosition()
 } // namespace player
 } // namespace tools
 
-System::System(entt::registry *registry) : registry(registry) {}
-
-void System::updatePlayer(entt::entity &entity)
+void System::updatePlayer(entt::registry &registry)
 {
-    // Get player entity position
-    auto &pos = this->registry->get<Position>(entity);
+    // Get registry components
+    auto view = registry.view<const std::string, Position>();
 
-    // Copy const player position
-    Position playerPos = pos;
+    // Filter out entity tags and update only the player
+    view.each(
+        // Loop components
+        [this, &registry](const auto entity, const auto &tag, auto &pos)
+        {
+            // Check if tag matches player tag
+            if (tag == PLAYER_TAG)
+            {
+                // Copy const player position
+                Position playerPos = pos;
 
-    // Get entity and mouse positions
-    Position mousePos = tools::player::getMousePosition();
+                // Get entity and mouse positions
+                Position mousePos = tools::player::getMousePosition();
 
-    // Distance between player and mouse position
-    int distanceX = std::abs(mousePos.x - playerPos.x);
-    int distanceY = std::abs(mousePos.y - playerPos.y);
+                // Distance between player and mouse position
+                int distanceX = std::abs(mousePos.x - playerPos.x);
+                int distanceY = std::abs(mousePos.y - playerPos.y);
 
-    // Initial player speed
-    int speedX = tools::player::SPEED;
-    int speedY = tools::player::SPEED;
+                // Initial player speed
+                int speedX = tools::player::SPEED;
+                int speedY = tools::player::SPEED;
 
-    // Set speed as difference in distance for the final x-axis movement
-    if (distanceX < speedX)
-    {
-        speedX = distanceX;
-    }
+                // Set speed as difference in distance for the final x-axis
+                // movement
+                if (distanceX < speedX)
+                {
+                    speedX = distanceX;
+                }
 
-    // y-axis final step
-    if (distanceY < speedY)
-    {
-        speedY = distanceY;
-    }
+                // y-axis final step
+                if (distanceY < speedY)
+                {
+                    speedY = distanceY;
+                }
 
-    // Move x-axis position
-    if (playerPos.x > mousePos.x)
-    {
-        playerPos.x -= speedX;
-    }
-    else if (playerPos.x < mousePos.x)
-    {
-        playerPos.x += speedX;
-    }
+                // Move x-axis position
+                if (playerPos.x > mousePos.x)
+                {
+                    playerPos.x -= speedX;
+                }
+                else if (playerPos.x < mousePos.x)
+                {
+                    playerPos.x += speedX;
+                }
 
-    // Move y-axis position
-    if (playerPos.y > mousePos.y)
-    {
-        playerPos.y -= speedY;
-    }
-    else if (playerPos.y < mousePos.y)
-    {
-        playerPos.y += speedY;
-    }
+                // Move y-axis position
+                if (playerPos.y > mousePos.y)
+                {
+                    playerPos.y -= speedY;
+                }
+                else if (playerPos.y < mousePos.y)
+                {
+                    playerPos.y += speedY;
+                }
 
-    // Update entity position
-    this->registry->replace<Position>(entity, playerPos);
+                // Update entity position
+                registry.replace<Position>(entity, playerPos);
+            }
+        });
+}
+
+// TODO: Finish this
+void System::updateEnemies(entt::registry &registry)
+{
+    // Loop over all enemy IDs
+    // for (const auto &e : enemies)
+    // {
+    //     // Get enemy entity position
+    //     auto &pos = this->registry.get<Position>(e);
+    //
+    //     // Copy const enemy position
+    //     Position enemyPos = pos;
+    // }
 }
