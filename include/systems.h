a6147cff54d134760d8ab340522fafd37a1c1ad8
Rakin Rahman <rakinhacker@gmail.com>
2022-09-22 11:09:03 +0600

    Add lastPlayerPos{}

--- include/systems.h
+++ include/systems.h
@@ -3,6 +3,8 @@
 
 #include <entt/entt.hpp>
 
+#include "components.h"
+
 // This class handles both the player and the enemy systems
 class System
 {
@@ -16,9 +18,10 @@ public:
     void updateEnemies();
 
 private:
-    static constexpr int PLAYER_RADIUS = 40;       // Default player radius
+    const int PLAYER_RADIUS = 40;                  // Default player radius
     static constexpr int TRIANGLE_SIDE_LENGTH = 5; // Triangle length
-    entt::registry *registry{};
+    entt::registry *registry{};                    // Main registry
+    Position lastPlayerPos{}; // Last position of last player
 };
 
 #endif
